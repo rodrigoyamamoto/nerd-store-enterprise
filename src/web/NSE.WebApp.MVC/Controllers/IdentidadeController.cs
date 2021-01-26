79e82e8eb526650345a8ec4b3955c30f0db08ce7
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-01-26 23:50:36 +0000

    feat: add login/create user views

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        //public IdentidadeController(IAutenticacaoService autenticacaoService)
+        //{
+        //    //_autenticacaoService = autenticacaoService;
+        //}
 
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //var resposta = await _autenticacaoService.Registro(usuarioRegistro);
+
+            //if (ResponsePossuiErros(resposta.ResponseResult)) return View(usuarioRegistro);
+
+            //await RealizarLogin(resposta);
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin, string returnUrl = null)
+        {
+            //ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //var resposta = await _autenticacaoService.Login(usuarioLogin);
+
+            //if (ResponsePossuiErros(resposta.ResponseResult)) return View(usuarioLogin);
+
+            //await RealizarLogin(resposta);
+
+            //if (string.IsNullOrEmpty(returnUrl)) return RedirectToAction("Index", "Home");
+
+            return LocalRedirect(returnUrl);
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+        private async Task RealizarLogin(UsuarioRespostaLogin resposta)
+        {
+            //var token = ObterTokenFormatado(resposta.AccessToken);
+
+            //var claims = new List<Claim>();
+            //claims.Add(new Claim("JWT", resposta.AccessToken));
+            //claims.AddRange(token.Claims);
+
+            //var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            //var authProperties = new AuthenticationProperties
+            //{
+            //    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+            //    IsPersistent = true
+            //};
+
+            //await HttpContext.SignInAsync(
+            //    CookieAuthenticationDefaults.AuthenticationScheme,
+            //    new ClaimsPrincipal(claimsIdentity),
+            //    authProperties);
+        }
+
+        //private static JwtSecurityToken ObterTokenFormatado(string jwtToken)
+        //{
+        //    //return new JwtSecurityTokenHandler().ReadToken(jwtToken) as JwtSecurityToken;
+        //}
     }
 }
