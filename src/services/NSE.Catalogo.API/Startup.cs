6ce6f879c9c27396afeafddc5f6107cbe9606277
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-01-27 18:27:15 +0000

    refact: extract Jwt validation to a new class lib as now it can exposes the validation to all availble apis in the solution

--- src/services/NSE.Catalogo.API/Startup.cs
+++ src/services/NSE.Catalogo.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Configuration;
+using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Catalogo.API
 {
@@ -31,6 +32,7 @@ namespace NSE.Catalogo.API
         {
             services.AddSwaggerConfiguration();
             services.AddApiConfiguration(Configuration);
+            services.AddJwtConfiguration(Configuration);
             services.RegisterServices();
         }
 
