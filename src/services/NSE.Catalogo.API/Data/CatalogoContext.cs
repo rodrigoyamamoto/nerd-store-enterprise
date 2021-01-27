0bf72497f1b25956c6d691ebcb2ac8c8ba2eeb9c
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-01-27 16:51:04 +0000

    chore: add database connection, table mapping and initial migration

---
+++ src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using System.Linq;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+    }
+}
--- src/services/NSE.Catalogo.API/Startup.cs
+++ src/services/NSE.Catalogo.API/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Catalogo.API.Data;
 
 namespace NSE.Catalogo.API
 {
@@ -15,13 +17,14 @@ namespace NSE.Catalogo.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
