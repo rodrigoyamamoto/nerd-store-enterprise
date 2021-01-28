cdc6cbc0e995499c1847eda4cffbd913fb61f096
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-01-28 04:02:37 +0000

    refact: major refactoring on files due change on project name, add configurations, mappings and cliente migration

---
+++ src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Catalogo.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            //services.AddScoped<CatalogoContext>();
+        }
+    }
+}
--- src/services/NSE.Cliente.API/Program.cs
+++ src/services/NSE.Cliente.API/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace NSE.Cliente.API
+namespace NSE.Clientes.API
 {
     public class Program
     {
