5deb03ea4168bd2d68cb9c0a97c204fb238772a8
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-03-04 22:34:46 +0000

    feat: add pagination

--- src/web/NSE.WebApp.MVC/wwwroot/js/script.js
+++ src/web/NSE.WebApp.MVC/wwwroot/js/script.js
@@ -4,21 +4,34 @@
 $(document).ready(function() {
 	// jQuery code
 
+
+
+    
     /* ///////////////////////////////////////
 
-    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE,
+    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE, 
     For sliders, interactions and other
 
     */ ///////////////////////////////////////
+    
 
 	//////////////////////// Prevent closing from click inside dropdown
     $(document).on('click', '.dropdown-menu', function (e) {
       e.stopPropagation();
     });
 
+
+    
+
 	//////////////////////// Bootstrap tooltip
 	if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
 		$('[data-toggle="tooltip"]').tooltip()
 	} // end if
-});
+
+
+
+
+    
+}); 
 // jquery end
+
